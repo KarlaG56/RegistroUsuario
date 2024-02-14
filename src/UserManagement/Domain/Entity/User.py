d466a73238dc6f5862d3827fa3b3c0535a5136ce
keyel <crugaldez13@gmail.com>
2024-02-13 22:00:36 -0600

    ADD

--- src/UserManagement/Domain/Entity/User.py
+++ src/UserManagement/Domain/Entity/User.py
@@ -1,13 +1,14 @@
-import string
 import uuid
+import bcrypt
 
-class User(object):
-    def __init__(self, name:string, lastname:string,cellphone: string,email: string,password: string):
-        self.id=str(uuid.uuid4())
-        self._name = name
-        self._lastname = lastname
-        self._cellphone = cellphone
-        self._email = email
-        self._password = password
-            
 
+class User:
+    def __init__(self, name, last_name, cellphone, email, password):
+        self.uuid = uuid.uuid4()
+        self.name = name
+        self.last_name = last_name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()).decode('utf8')
+        self.cellphone = cellphone
+        self.activation_token = None
+        self.activated_at = None
